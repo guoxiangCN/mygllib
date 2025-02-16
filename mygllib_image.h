de8a3569be9a018c5172f416a5df0f855d5ee4be
mzygQAQ <mzygQAQ@noreply.gitcode.com>
2025-02-16 15:47:29 +0800

    format with google style

--- mygllib_image.h
+++ mygllib_image.h
@@ -1,48 +1,43 @@
 #pragma once
 
-
 #include "mygllib_types.h"
 
 namespace mygl {
 
-	class Image {
-	public:
-		Image(int width, int height, byte* data)
-			: m_width(width), m_height(height), m_data(nullptr) {
-			if (data) {
-				auto data_ptr = new RGBA[width * height];
-				memcpy(data_ptr, data, sizeof(RGBA) * height * width);
-				m_data.reset(data_ptr);
-			}
-		}
+class Image {
+public:
+    Image(int width, int height, byte* data)
+        : m_width(width), m_height(height), m_data(nullptr) {
+        if (data) {
+            auto data_ptr = new RGBA[width * height];
+            memcpy(data_ptr, data, sizeof(RGBA) * height * width);
+            m_data.reset(data_ptr);
+        }
+    }
 
-		~Image() = default;
+    ~Image() = default;
 
-		int GetWidth() const {
-			return m_width;
-		}
+    int GetWidth() const { return m_width; }
 
-		int GetHeight() const {
-			return m_height;
-		}
+    int GetHeight() const { return m_height; }
 
-		RGBA GetColor(int x, int y) const {
-			if (x < 0 || x >= m_width || y < 0 || y >= m_height) {
-				return RGBA(0, 0, 0, 0);
-			}
-			return m_data[static_cast<size_t>(y * m_width + x)];
-		}
+    RGBA GetColor(int x, int y) const {
+        if (x < 0 || x >= m_width || y < 0 || y >= m_height) {
+            return RGBA(0, 0, 0, 0);
+        }
+        return m_data[static_cast<size_t>(y * m_width + x)];
+    }
 
-	private:
-		int m_width;
-		int m_height;
-		std::unique_ptr<RGBA[]> m_data;
-	};
+private:
+    int m_width;
+    int m_height;
+    std::unique_ptr<RGBA[]> m_data;
+};
 
-	/**
-	 * @brief Read a image from a pic file(jpg jpeg...)
-	 * @param path 
-	 * @return 
-	 */
-	Image* ReadImageFromFile(const std::string& path);
-}
+/**
+ * @brief Read a image from a pic file(jpg jpeg...)
+ * @param path
+ * @return
+ */
+Image* ReadImageFromFile(const std::string& path);
+}  // namespace mygl
--- mygllib_types.h
+++ mygllib_types.h
@@ -1,44 +1,41 @@
 #pragma once
 
+#include <cstring>
 #include <memory>
 #include <string>
-#include <cstring>
 
 namespace mygl {
 
-	using uint = unsigned int;
-	using byte = unsigned char;
+using uint = unsigned int;
+using byte = unsigned char;
 
-	template<typename T>
-	struct Vec2 {
-		T x;
-		T y;
+template <typename T>
+struct Vec2 {
+    T x;
+    T y;
 
-		Vec2(T _x, T _y) {
-			x = _x;
-			y = _y;
-		}
+    Vec2(T _x, T _y) {
+        x = _x;
+        y = _y;
+    }
 
-		Vec2() {
-			x = -1;
-			y = -1;
-		}
-	};
+    Vec2() {
+        x = -1;
+        y = -1;
+    }
+};
 
-	using Point2D = Vec2<int>;
+using Point2D = Vec2<int>;
 
-	struct RGBA {
-		// b,g,r,a is ordered, Suiting for the Win32 Draw API, DO NOT change.
-		byte m_b;  // blue
-		byte m_g;  // gree
-		byte m_r;  // red
-		byte m_a;  // alpha
+struct RGBA {
+    // b,g,r,a is ordered, Suiting for the Win32 Draw API, DO NOT change.
+    byte m_b;  // blue
+    byte m_g;  // gree
+    byte m_r;  // red
+    byte m_a;  // alpha
 
-		RGBA(byte r = 255, byte g = 255, byte b = 255, byte a = 255)
-			: m_r(r), m_g(g), m_b(b), m_a(a) {
-		}
-	};
+    RGBA(byte r = 255, byte g = 255, byte b = 255, byte a = 255)
+        : m_r(r), m_g(g), m_b(b), m_a(a) {}
+};
 
-}
-
-
+}  // namespace mygl
